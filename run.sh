23f3689141102514e88c823d25f23ffce46dd109
LZYSaltedFish <3050669253@qq.com>
2023-05-10 14:21:05 +0800

    new

---
+++ run.sh
@@ -0,0 +1,2 @@
+python main.py \
+    --log_dir='/root/data/tmp_dlc/log'
--- test.py
+++ test.py
@@ -1,13 +1,21 @@
 from torch.utils.tensorboard import SummaryWriter
 import random
+import fire
+from tqdm import tqdm
 
-tensorboard = SummaryWriter(
-    log_dir='./log'
-)
+def main(
+    log_dir='/root/data/tmp_dlc/log'
+):
+    tensorboard = SummaryWriter(
+        log_dir=log_dir
+    )
 
-loss = 3.13
-for step in range(1000):
-    loss += random.random()-0.7
-    tensorboard.add_scalar(tag='train/loss',
-                        scalar_value=loss,
-                        global_step=step)
+    loss = 3.13
+    for step in tqdm(range(1000)):
+        loss += random.random()-0.7
+        tensorboard.add_scalar(tag='train/loss',
+                            scalar_value=loss,
+                            global_step=step)
+    
+if __name__ == '__main__':
+    fire.Fire(main)
